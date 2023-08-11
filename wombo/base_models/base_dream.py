d3616a6a3e9871dda22a1b952a725f43b4e87f4d
Garifullin Marsel <garifyllinmarsel197688@gmail.com>
2023-08-12 02:53:05 +0400

    refactoring BaseDream class

--- wombo/base_models/base_dream.py
+++ wombo/base_models/base_dream.py
@@ -1,18 +1,22 @@
-from typing import List
 from io import BytesIO
+from abc import ABC, abstractmethod
+from typing import List, Union
 
 from PIL.Image import Image
 from httpx._client import BaseClient
 
+from wombo.models import CheckTask, CreateTask
 
-class BaseDream:
+
+class BaseDream(ABC):
     """
     Base class for all dreams (sync / async).
     """
 
     client: BaseClient
 
-    def save_frames_as_gif(self, frames: List[Image], duration: int = 400) -> BytesIO:
+    @staticmethod
+    def save_frames_as_gif(frames: List[Image], duration: int = 400) -> BytesIO:
         """
         Saves a list of frames as a gif and returns as bytes IO for writing stream.
         """
@@ -28,6 +32,34 @@ class BaseDream:
         )
 
         return result
-    
-    def generate(self):
-        raise NotImplementedError("Method not Implemented")
+
+    @abstractmethod
+    def _get_js_filename(self) -> str:
+        pass
+
+    @abstractmethod
+    def _get_google_key(self) -> str:
+        pass
+
+    @abstractmethod
+    def _get_auth_key(self) -> str:
+        pass
+
+    @abstractmethod
+    def create_task(self, text: str, style: int) -> CreateTask:
+        pass
+
+    @abstractmethod
+    def check_task(self, task_id: str, only_bool: bool) -> Union[CheckTask, bool]:
+        pass
+
+    @abstractmethod
+    def generate(
+            self,
+            text: str,
+            style: int,
+            gif: bool,
+            timeout: int,
+            check_for: int
+    ) -> Union[BytesIO, Union[CheckTask, bool]]:
+        pass
