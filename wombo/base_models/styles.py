29a8a63aed2874331d7a77827d3b6cff18b0c821
Garifullin Marsel <garifyllinmarsel197688@gmail.com>
2023-08-12 03:12:12 +0400

    add enum styles

--- wombo/base_models/styles.py
+++ wombo/base_models/styles.py
@@ -1,80 +1,60 @@
-from wombo.base_models import BaseDream
-from wombo.api import Dream, AsyncDream
+from enum import Enum
 
-styles_list = {
-    3: 'no_style', 
-    9: 'psychic', 
-    14: 'etching',
-    16: 'wuhtercuhler', 
-    17: 'provenance', 
-    18: 'rose_gold', 
-    22: 'ghibli', 
-    28: 'melancholic', 
-    31: 'toasty', 
-    32: 'realistic', 
-    34: 'arcane', 
-    35: 'throwback', 
-    36: 'daydream', 
-    37: 'surreal', 
-    38: 'ink', 
-    39: 'pandora', 
-    40: 'malevolent', 
-    41: 'street_art', 
-    42: 'unrealistic', 
-    45: 'comic', 
-    46: 'anime', 
-    47: 'line_art', 
-    48: 'gouache', 
-    49: 'polygon', 
-    50: 'paint', 
-    52: 'hdr', 
-    53: 'analogue', 
-    54: 'retro_futurism', 
-    55: 'isometric', 
-    57: 'bad_trip', 
-    58: 'cartoonist', 
-    60: 'vector', 
-    61: 'fantastical', 
-    63: 'Spectral', 
-    65: 'diorama', 
-    67: 'abstract', 
-    68: 'flora', 
-    71: 'soft_touch', 
-    72: 'winter', 
-    73: 'festive', 
-    74: 'splatter', 
-    76: 'figure', 
-    77: 'expressionism', 
-    78: 'realistic_v2', 
-    80: 'anime_v2', 
-    81: 'flora_v2', 
-    84: 'buliojourney_v2', 
-    88: 'blues_v2', 
-    91: 'watercolor_v2', 
-    92: 'spectral_v2', 
-    94: 'gloomy', 
-    95: 'the_cut', 
-    96: 'the_bulio_cut', 
-    97: 'dreamwave_v2', 
-    98: 'illustrated_v2', 
-    99: 'abstract_fluid_v2'
-}
 
-class Style:
-    def __init__(self, dream: BaseDream = None) -> None:
-        self.dream = dream
-    def __getattr__(self, param: str):
-        if param.isupper():
-            return list(styles_list.keys())[list(styles_list.values()).index(param.lower())]
-        else:
-            if self.dream is not None:
-                if isinstance(self.dream, Dream):
-                    def sync_func(text: str):
-                        return self.dream.generate(text=text, style=list(styles_list.keys())[list(styles_list.values()).index(param.lower())])
-                    return sync_func
-                elif isinstance(self.dream, AsyncDream):
-                    async def async_func(text: str):
-                        return self.dream.generate(text=text, style=list(styles_list.keys())[list(styles_list.values()).index(param.lower())])
-                    return async_func
-                else:
-                    return None
+class Style(Enum):
+    no_style = 3
+    psychic = 9
+    etching = 14
+    wuhtercuhler = 16
+    provenance = 17
+    rose_gold = 18
+    ghibli = 22
+    melancholic = 28
+    toasty = 31
+    realistic = 32
+    arcane = 34
+    throwback = 35
+    daydream = 36
+    surreal = 37
+    ink = 38
+    pandora = 39
+    malevolent = 40
+    street_art = 41
+    unrealistic = 42
+    comic = 45
+    anime = 46
+    line_art = 47
+    gouache = 48
+    polygon = 49
+    paint = 50
+    hdr = 52
+    analogue = 53
+    retro_futurism = 54
+    isometric = 55
+    bad_trip = 57
+    cartoonist = 58
+    vector = 60
+    fantastical = 61
+    Spectral = 63
+    diorama = 65
+    abstract = 67
+    flora = 68
+    soft_touch = 71
+    winter = 72
+    festive = 73
+    splatter = 74
+    figure = 76
+    expressionism = 77
+    realistic_v2 = 78
+    anime_v2 = 80
+    flora_v2 = 81
+    buliojourney_v2 = 84
+    blues_v2 = 88
+    watercolor_v2 = 91
+    spectral_v2 = 92
+    gloomy = 94
+    the_cut = 95
+    the_bulio_cut = 96
+    dreamwave_v2 = 97
+    illustrated_v2 = 98
+    abstract_fluid_v2 = 99
