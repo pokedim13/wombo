6962bcad98e0218fbae2ab4884da697d1c3f4764
Дмитрий Бондарев <baev@cumproject.ru>
2024-10-02 02:00:15 +0000

    add example.py

--- tests/example.py
+++ tests/example.py
@@ -1,2 +1,11 @@
 from wombo import AsyncDream, Dream
+import asyncio
 
+async def main():
+    dream = AsyncDream()
+    print(await dream.generate("anime waifu"))
+
+
+dream = Dream()
+print(dream.generate("anime waifu"))
+asyncio.run(main())
